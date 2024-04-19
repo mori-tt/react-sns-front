173029f18cebfc2f7f3eb951bbd27f779d5861c0
T_Mori <morit0301@gmail.com>
2024-04-19 11:04:27 +0900

    💄 jsx,css_topbar,sidebar

---
+++ src/components/timeline/Timeline.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Timeline.css";
+
+export default function Timeline() {
+  return (
+    <div className="timeline">
+      <div className="timelineWrapper">
+        <Share />
+        {/* <Post /> */}
+      </div>
+    </div>
+  );
+}
